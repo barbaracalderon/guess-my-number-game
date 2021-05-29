97761c46f2bbd979f4296d73bacc8cfe81608c1a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-05-29 16:38:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# guess-my-number-game
+ Guess a number from 0 to 50 and keep track of your score. Adivinhe um número de 0 a 50 e fique ligado na sua pontuação.
---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let score = 20;
+let secretNumber = Math.trunc(Math.random()*50)+1;
+let highscore = 0;
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+const changeBackground = function (color) {
+    document.querySelector('body').style.backgroundColor = color;
+}
+const displayQuestionMark = function (message) {
+    document.querySelector('.number').textContent = message;
+}
+const displayScore = function (score) {
+    document.querySelector('.score').textContent = score;
+}
+const displayHighscore = function (highscore) {
+    document.querySelector('.highscore').textContent = highscore;
+}
+
+displayQuestionMark('?');
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    if (score > 1) {
+        if (!guess) {
+            displayMessage('📛 No number!');
+        } else if (guess === secretNumber) {
+            displayQuestionMark(secretNumber)
+            displayMessage('🎆 Hooray! You did it!');
+            changeBackground('#60b347');
+            document.querySelector('.number').style.width = '40rem';
+            if (score > highscore) {
+                highscore = score;
+                displayHighscore(Number(highscore));
+            }
+        } else if (guess !== secretNumber) {
+            displayMessage(guess > secretNumber ? '🚫 Too high!' : '🚫 Too low!');
+            score--;
+            displayScore(score);
+        }
+    } else {
+        displayScore(Number('0'));
+        displayMessage('Game Over!');
+        changeBackground('#cc0000')
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 50) + 1;
+    displayMessage('Start guessing...');
+    displayScore(score);
+    displayQuestionMark('?');
+    document.querySelector('.guess').value = '';
+    changeBackground('#222');
+    document.querySelector('.number').style.width = '15rem';
+});
